f3cadcc01eef2c9dfd79dfb9104b26bb309f9b57
ji seok Lee <ljs94920@gmail.com>
2026-08-31 17:42:19 +0900

    Update keras06_batch.py

--- keras/keras06_batch.py
+++ keras/keras06_batch.py
@@ -11,13 +11,15 @@ y = np.array([1,2,3,5,4,6])
 model = Sequential()
 model.add(Dense(3, input_dim=1))
 model.add(Dense(5))
-model.add(Dense(4))
+model.add(Dense(6))
+model.add(Dense(9))
+model.add(Dense(5))
 model.add(Dense(1))
 
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=1000, batch_size=4)
+model.fit(x, y, epochs=10000)#, batch_size=3)
 
 #4. 평가, 예측
 loss = model.evaluate(x, y)
