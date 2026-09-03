2a0d746e1d7b55d7694124ef388be961968de2cc
ji seok Lee <ljs94920@gmail.com>
2026-09-03 17:44:57 +0900

    기초 MLP 실습(따릉이 data)

---
+++ keras/keras13_ddarung.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+
+#0. RMSE 함수 작성
+def RMSE(y_test, y_predict):
+    rmse = np.sqrt(mean_squared_error(y_test,y_predict))
+    return rmse
+
+
+#1. 데이터
+path = "./_data/ddarung/"
+
+train_csv = pd.read_csv(path + "train.csv", index_col=0)  #index_col = 0 : 첫번째 coloumn을 index 처리하여 data로 취급하지 않음
+print(train_csv)            #[1459 rows x 10 columns]
+
+test_csv = pd.read_csv(path + "test.csv", index_col=0)
+print(test_csv)             #[715 rows x 9 columns]
+
+submission = pd.read_csv(path + "submission.csv" , index_col=0)
+print(submission)           #[715 rows x 1 columns]
+
+print(train_csv.info()) # pandas로 불러온 data의 information ex) 결측치 개수 등을 보여줌
+print(test_csv.info())
+
+# exit()
+######################### 결측치 처리 1. 삭제 ####################################
+train_csv = train_csv.dropna()
+print(train_csv)            #[1328 rows x 10 columns]
+
+# train_csv를 x와 y로 분리
+x = train_csv.drop(['count'], axis=1) #열 삭제)
+
+y = train_csv['count']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=915124)
+
+#2. 모델 구성
+model = Sequential()
+model.add(Dense(45, input_dim=9))
+model.add(Dense(35))
+model.add(Dense(27))
+model.add(Dense(13))
+model.add(Dense(9))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, epochs=1500, batch_size=100 )
+
+#4. 평가, 예측
+loss = model.evaluate(x_test, y_test)
+y_predict = model.predict(x_test)
+rmse = RMSE(y_test, y_predict)
+print("RMSE value : ", rmse)
+
