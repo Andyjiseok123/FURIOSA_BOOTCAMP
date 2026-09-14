532918fd3976950ff4c6c763684bd533fe556a9e
ji seok Lee <ljs94920@gmail.com>
2026-09-14 17:32:05 +0900

    funtional model

---
+++ keras/keras34_hamsu01_california.py
@@ -0,0 +1,115 @@
+#29-6 카피
+
+# import ssl
+# ssl._create_default_https_context = ssl.create_default_context 다운로드 안될떄 사용할것
+from sklearn.datasets import fetch_california_housing
+from tensorflow.keras.models import Sequential,load_model
+from tensorflow.keras.layers import Dense, Dropout
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
+import numpy as np
+import time
+
+# path = './_save/keras30/'
+
+#1.데이터 
+datasets = fetch_california_housing()
+x = datasets.data
+y = datasets.target
+
+x_train,x_test,y_train,y_test = train_test_split(
+    x,y,
+    train_size=0.75,
+    random_state=333
+)
+
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler  #preprocessing(전처리)
+from sklearn.preprocessing import RobustScaler
+##############################################################################
+# scaler = MinMaxScaler()
+##############################################################################
+
+##############################################################################
+# scaler = StandardScaler()
+##############################################################################
+
+##############################################################################
+# scaler = MaxAbsScaler()
+##############################################################################
+
+##############################################################################
+scaler = RobustScaler()
+##############################################################################
+# 이상치에 강력함
+
+##############################################################################
+x_train = scaler.fit_transform(x_train)
+##############################################################################
+x_test = scaler.transform(x_test) 
+
+#2.모델구성
+model = Sequential()
+model.add(Dense(9, input_dim=8,activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(9,activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(12,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(9,activation='relu'))
+model.add(Dense(5,activation='relu'))
+model.add(Dense(1))
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+#3.컴파일,훈련
+model.compile(loss='mse', optimizer= 'adam')
+
+es = EarlyStopping(monitor='val_loss',
+                   mode='min',
+                   patience=30,
+                   restore_best_weights=True,
+                   verbose=1
+                   )
+
+# mcp = ModelCheckpoint(monitor='val_loss',
+#                       mode='auto',
+#                       save_best_only=True,
+#                       filepath=path + 'keras30_mcp1.keras',
+#                       verbose=1
+#                       )
+
+strat_time = time.time()  #현재 시간을 반환 ,시작시간
+hist = model.fit(x_train,y_train, 
+                 epochs=1000, 
+                 batch_size=64,
+                 validation_split=0.2,
+                 callbacks = [es],
+                 verbose = 1)
+end_time = time.time()  #훈련 끝난 시간을 반환 , 끝시간
+
+#######################################################
+# model.save(path + 'keras29_3_save_model.keras') #가중치 세이브
+#######################################################
+
+#4.평가 ,예측
+loss = model.evaluate(x_test,y_test)
+print("loss:", loss)
+
+y_predict = model.predict(x_test)
+r2 = r2_score(y_test, y_predict) 
+print('r2결과값: ' ,r2)
+
+mse = mean_squared_error(y_test,y_predict)
+print('mse : ', mse)
+
+def RMSE(y_test, y_predict):  #RMSE 함수정의
+    return np.sqrt(mean_squared_error(y_test,y_predict))  #np.sqrt하면 mse에 루트가 씌워짐
+
+rmse = RMSE(y_test, y_predict)
+
+print('RMSE : ', rmse) 
+
+
+
+# r2결과값:  0.7698115672891012
+# mse :  0.2945619896559895
+# RMSE :  0.5427356535699396
---
+++ keras/keras34_hamsu10_digits.py
@@ -0,0 +1,142 @@
+from sklearn.datasets import load_digits
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential, load_model, Model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import Dense, Dropout, Input
+import time
+from sklearn.metrics import accuracy_score
+import os
+import datetime
+path = './_save/keras31/digits/'
+date = datetime.datetime.now()
+date = date.strftime("%m%d_%H%M")
+filename = '{epoch:04d}-{val_loss:4f}.keras'
+filepath = "".join([path,"k31_",date,filename])
+
+#1데이터
+
+datasets = load_digits()
+# print(datasets)
+
+
+x = datasets.data
+y = datasets.target
+# print(x.shape,y.shape) #(1797, 64) (1797,)
+
+y = pd.get_dummies(y,dtype=int)
+# print(y)
+
+# print(np.unique(y))  #[0 1]
+# print(np.unique(y,return_counts=True)) #(array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), array([178, 182, 177, 183, 181, 182, 181, 179, 174, 180]))
+# print(pd.Series(y).value_counts())
+
+x_train,x_test,y_train,y_test =train_test_split(
+    x,y,
+    random_state=333,
+    train_size=0.8,
+    stratify=y,  #x,y데이터를 나눌떄 stratify=y이걸안넣으면 x,y서로 데이터 크기가 달랐을떄 비율편차가 생길수있음
+)
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler 
+from sklearn.preprocessing import RobustScaler
+##############################################################################
+# scaler = MinMaxScaler()
+##############################################################################
+
+
+
+##############################################################################
+# scaler = StandardScaler()
+##############################################################################
+
+
+
+##############################################################################
+# scaler = MaxAbsScaler()
+##############################################################################
+
+
+##############################################################################
+scaler = RobustScaler()
+##############################################################################
+scaler.fit(x_train) # x 값을  MinMaxScaler으로 실행시킬 준비
+x_train = scaler.fit_transform(x_train) # 0~1 값 변환 사이로변환
+x_test = scaler.transform(x_test) 
+
+#2.모델구성
+# model = Sequential()
+# model.add(Dense(30, input_dim=64, activation= 'relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(50, actzvation= 'relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(100,activation= 'relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(50, activation= 'relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(30, activation= 'relu'))
+# model.add(Dense(10,activation='softmax'))
+
+input1 = Input(shape=(64,))
+dense1 = Dense(30,activation = 'relu',name='ys1')(input1)
+drop1 = Dropout(0.2)(dense1)
+dense2 = Dense(50,activation = 'relu', name= 'ys2')(drop1)
+drop2 = Dropout(0.2)(dense2)
+dense3 = Dense(100,activation = 'relu', name= 'ys3')(drop2)
+drop3 = Dropout(0.2)(dense3)
+dense4 = Dense(50,activation = 'relu', name= 'ys4')(drop3)
+drop4 = Dropout(0.2)(dense4)
+dense5 = Dense(30,activation = 'relu', name= 'ys5')(drop4)
+output1 = Dense(10,activation='softmax')(dense5)
+
+model = Model(inputs=input1, outputs=output1)
+
+#3.컴파일,훈련
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = 'adam',
+              metrics =['acc']
+              )
+es = EarlyStopping(
+    monitor= 'val_loss',
+    mode= 'auto',
+    patience=200,
+    restore_best_weights=True,
+)
+mcp = ModelCheckpoint(monitor='val_loss',
+                      mode='auto',
+                      save_best_only=True,
+                      filepath = filepath,
+                      verbose=1
+                      )
+start_time =time.time()
+model.fit(x_train,y_train, epochs=500,batch_size=300,
+          verbose=1,
+          validation_split=0.3,
+          callbacks =[es,mcp],
+          )
+end_time =time.time()
+
+result = model.evaluate(x_test,y_test,)
+print('loss: ',result[0])
+print('acc: ',round(result[1],2))
+y_predict= model.predict(x_test) 
+
+y_predict = np.argmax(y_predict,axis=1) 
+# print(y_predict)#[0 2 0 1 1 2 0 2 0 2 2 1 2 0 0 0 2 0 2 1 0 2 1 1 0 2 1 1 1 2]
+y_test = np.argmax(y_test, axis=1)
+# print(y_test) #[0 2 0 1 1 1 0 2 0 2 2 2 2 0 0 0 2 0 2 1 0 2 1 1 0 2 1 1 1 1]
+# #######################################################
+# y_predict = np.argmax(model.predict(x_test),axis =1)
+# y_test_argmax =np.argmax(y_test,axis=1)
+# ########################################################
+# y_predict = model.predict(x_test)
+# print(y_predict)
+
+
+accuracy_score =accuracy_score(y_test,y_predict)  
+#지금까지는 y_predict 값은 [0.7,0.2,0.1]이런식으로 되어있어서 비교가 불가능함 >>가장큰 수를 1로 바꿔줘야함 그래서 결과를 [1,0,0]으로 변경후 비교 
+print('acc_score :',accuracy_score)
+print('걸린시간: ', round(end_time-start_time, 2),'초')
+
+# acc_score : 0.95
+# 걸린시간:  17.46 초
