97ad63b10e6c45154d6fc430e24c263b41f939e5
ji seok Lee <ljs94920@gmail.com>
2026-09-23 17:43:50 +0900

    Update keras55_LSTM1_summary.py

--- keras/keras55_LSTM1_summary.py
+++ keras/keras55_LSTM1_summary.py
@@ -24,7 +24,7 @@ x = x.reshape(x.shape[0],
 #2. 모델 구성
 model = Sequential()
 # model.add(SimpleRNN(units=10, input_shape=(3,1)))       # 3차원으로 들어가서 2(1)차원으로 나옴 -> 바로 Dense와 연결가능
-model.add(LSTM(units=10, input_shape=(3,1)))                
+model.add(LSTM(units=3, input_shape=(3,1)))                
 model.add(Dense(7,activation='relu'))
 model.add(Dense(1))
 
@@ -32,17 +32,49 @@ model.summary()
 # 파라미터의 갯수 = units*(feature + bias + units)
 exit()
 
-"""units = 10
+"""LSTM
  Layer (type)                Output Shape              Param #   
 =================================================================
- simple_rnn (SimpleRNN)      (None, 10)                120       
+ simple_rnn (SimpleRNN)      (None, 10)                480       
  dense (Dense)               (None, 7)                 77        
  dense_1 (Dense)             (None, 1)                 8         
 =================================================================
 Total params: 205
 Trainable params: 205
 Non-trainable params: 0
+
+_________________________________________________________________
+ Layer (type)                Output Shape              Param #   
+=================================================================
+ lstm (LSTM)                 (None, 1)                 12        
+                                                                 
+ dense (Dense)               (None, 7)                 14        
+                                                                 
+ dense_1 (Dense)             (None, 1)                 8         
+                                                                 
+=================================================================
+Total params: 34
+Trainable params: 34
+Non-trainable params: 0
+
+_________________________________________________________________
+ Layer (type)                Output Shape              Param #   
+=================================================================
+ lstm (LSTM)                 (None, 2)                 32        
+                                                                 
+ dense (Dense)               (None, 7)                 21        
+                                                                 
+ dense_1 (Dense)             (None, 1)                 8         
+                                                                 
+=================================================================
+Total params: 61
+Trainable params: 61
+Non-trainable params: 0
+
 """
+
+
+
 
 """
 가중치의 누적때문에 RNN은 초기 time step때의 데이터가 손실된다
